ae9c61146dd9f3c546538180d9f900a8eaed3eca
BrandonBian <71237855+BrandonBian@users.noreply.github.com>
2023-04-18 23:00:43 -0400

    actually sending entire replay buffer

--- distrib_l2r/asynchron/learner.py
+++ distrib_l2r/asynchron/learner.py
@@ -207,7 +207,7 @@ class AsyncLearningNode(ThreadPoolMixIn, socketserver.TCPServer):
         while True:
             try:
                 logging.warning("trying to copy")
-                buffer_to_send = deepcopy(sending)
+                buffer_to_send = deepcopy(self.replay_buffer)
                 break
             except RuntimeError as e:
                 logging.warning(e)
