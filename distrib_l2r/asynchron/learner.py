e5fda6fe266a46a1eee4bdc19093003ddbd1983e
BrandonBian <71237855+BrandonBian@users.noreply.github.com>
2023-04-21 11:15:00 -0400

    sleeping in learn() for 10 seconds

--- distrib_l2r/asynchron/learner.py
+++ distrib_l2r/asynchron/learner.py
@@ -284,7 +284,7 @@ class AsyncLearningNode(ThreadPoolMixIn, socketserver.TCPServer):
                 # Add new data to the primary replay buffer
                 self.replay_buffer.store(semibuffer)
             
-            time.sleep(1)
+            time.sleep(10)
 
         # epoch = 0
         # while True:
